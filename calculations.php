29b98266ed7f5a80b22ddb510ea2c2fd20eb0efb
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-03-13 16:50:06 -0400

    Update calculations.php

--- calculations.php
+++ calculations.php
@@ -1,1 +1,11 @@
-
+<?php
+if ( isset( $_POST['diameter'] ) ){
+    $diameter = $_POST['diameter'];
+    $pi = 3.14159;
+    $radius = $diameter / 2;
+    $circumference = 2 * $pi * $radius;
+    $area = $pi * $radius * $radius;
+    echo "The circumference is: " . $circumference . "<br>";
+    echo "The area is: " . $area;
+    
+}
