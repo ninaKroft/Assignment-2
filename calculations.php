ce93f86e2c7f3000698175f5ab7c3cd37ba8b631
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-03-31 14:49:33 +0000

    favicon

--- calculations.php
+++ calculations.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 <link rel="stylesheet" href="stylesheet.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png" />
+<link rel="manifest" href="./images/favicon/site.webmanifest" />
 </head>
 <body>
 <center><h1 class ="title">Here are your results!</h1><center>
